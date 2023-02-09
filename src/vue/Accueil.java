00188af5466b1c437fd3c67eb51069a7415ca2f2
Charles Fouquet <fouquetcharles@gmail.com>
2023-02-09 14:10:39 +0100

    Push Header

---
+++ src/vue/Accueil.java
@@ -0,0 +1,137 @@
+package vue;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Accueil extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2798845607393016064L;
+	private JTextField searchField;
+
+	/**
+	 * Create the panel.
+	 */
+	public Accueil() {
+		setBounds(0, 0, 1000, 600);
+		setLayout(null);
+		
+		Font mf = new Font("Noto Serif", Font.BOLD, 15);
+		UIManager.put("Menu.font", mf);
+		Font mif = new Font("Noto Serif", Font.PLAIN, 12);
+		UIManager.put("MenuItem.font", mif);
+		
+		JPanel menu = new JPanel();
+		menu.setBounds(320, 0, 250, 300);
+		menu.setBackground(new Color(0, 0, 0, 0));
+		add(menu);
+		menu.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(new Color(243, 232, 207));
+		menuBar.setBounds(0, 10, 100, 30);
+//		menuBar.setLayout(new GridBagLayout());
+		menu.add(menuBar);
+		
+		JMenu menuBtn = new JMenu("Menu");
+		menuBtn.setBackground(new Color(255, 204, 51));
+		menuBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(menuBtn);
+		
+		JMenuItem menuGenres = new JMenuItem("Auteurs");
+		menuBtn.add(menuGenres);
+		
+		JMenuItem menuAuteurs = new JMenuItem("Genres");
+		menuBtn.add(menuAuteurs);
+		
+		JMenuItem menuSeries = new JMenuItem("Series");
+		menuBtn.add(menuSeries);
+		
+		JPanel body = new JPanel();
+		body.setBackground(new Color(240, 227, 198));
+		body.setBounds(0, 50, 1000, 550);
+		add(body);
+		body.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(156, 225, 321, 164);
+		body.add(panel);
+		
+		JPanel header = new JPanel();
+		header.setBackground(new Color(248, 243, 231));
+		header.setBounds(0, 0, 1000, 50);
+		add(header);
+		header.setLayout(null);
+		
+		JLabel logo = new JLabel(new ImageIcon("src/images/home_30x30.png"));
+		logo.setBounds(10, 10, 32, 32);
+		header.add(logo);
+		
+		JLabel homeName = new JLabel("Bibliothèque");
+		homeName.setVerticalAlignment(JLabel.CENTER);
+		homeName.setFont(new Font("Noto Serif", Font.PLAIN, 25));
+		homeName.setBounds(50, 0, 175, 50);
+		header.add(homeName);
+		
+		searchField = new JTextField();
+		searchField.setBounds(435, 15, 300, 20);
+		header.add(searchField);
+		searchField.setColumns(10);
+		
+		JButton searchBtn = new JButton("Rechercher");
+		searchBtn.setBackground(new Color(243, 232, 207));
+		searchBtn.setFont(new Font("Noto Serif", Font.BOLD, 11));
+		searchBtn.setBounds(735, 15, 100, 19);
+		header.add(searchBtn);
+		
+		JPanel userAccount = new JPanel();
+		userAccount.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				body.removeAll();
+				body.add(new Login());
+				body.repaint();
+				body.revalidate();
+			}
+		});
+		userAccount.setBounds(850, 0, 150, 50);
+		userAccount.setBackground(new Color(255, 255, 255));
+		header.add(userAccount);
+		userAccount.setLayout(null);
+		
+		JLabel userIcon = new JLabel(new ImageIcon("src/images/user.png"));
+		userIcon.setBounds(10, 10, 32, 32);
+		userAccount.add(userIcon);
+		
+		JLabel connectionStatus1 = new JLabel("Connexion");
+		connectionStatus1.setHorizontalAlignment(SwingConstants.CENTER);
+		connectionStatus1.setFont(new Font("Noto Serif", Font.BOLD, 13));
+		connectionStatus1.setForeground(new Color(199, 152, 50));
+		connectionStatus1.setBounds(52, 10, 90, 15);
+		userAccount.add(connectionStatus1);
+		
+		JLabel connectionStatus2 = new JLabel("Inscription");
+		connectionStatus2.setHorizontalAlignment(SwingConstants.CENTER);
+		connectionStatus2.setFont(new Font("Noto Serif", Font.BOLD, 13));
+		connectionStatus2.setForeground(new Color(199, 152, 50));
+		connectionStatus2.setBounds(52, 25, 90, 15);
+		userAccount.add(connectionStatus2);
+
+	}
+}
--- src/vue/Main.java
+++ src/vue/Main.java
@@ -1,11 +1,25 @@
 package vue;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class Main extends JFrame {
 
@@ -27,9 +41,38 @@ public class Main extends JFrame {
 					frame.getContentPane().setPreferredSize(new Dimension(1000, 600));
 					frame.pack();
 					frame.getContentPane().removeAll();
-//					frame.getContentPane().add();
+					frame.getContentPane().add(new Accueil());
 					frame.getContentPane().repaint();
 					frame.getContentPane().revalidate();
+					
+					frame.setLocationRelativeTo(null);
+					
+					frame.setTitle("Bibliothèque");
+					
+					List<Image> icons = new ArrayList<Image>();
+					icons.add(new ImageIcon("src/images/home_16x16.png").getImage());
+					icons.add(new ImageIcon("src/images/home_32x32.png").getImage());
+					icons.add(new ImageIcon("src/images/home_64x64.png").getImage());
+					icons.add(new ImageIcon("src/images/home_128x128.png").getImage());
+					frame.setIconImages(icons);
+					
+					InputStream fontFile = null;
+					Font noto = null;
+					try {
+						fontFile = new BufferedInputStream(Files.newInputStream(Paths.get("src/font/NotoSerif-Regular.ttf")));
+						try {
+							noto = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+							GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+							genv.registerFont(noto);
+						} catch (FontFormatException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,12 +85,22 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 128, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(255, 0, 0)));
+		panel.setBackground(new Color(255, 128, 128));
+		panel.setBounds(0, 0, 600, 400);
+		contentPane.add(panel);
+		
 	}
 
 }
