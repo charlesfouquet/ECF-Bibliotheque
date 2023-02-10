c820a8be5438061a4fe52f78b2dbd8dfcce723f6
Charles Fouquet <fouquetcharles@gmail.com>
2023-02-10 11:49:19 +0100

    Update Tri.java

--- src/vue/Tri.java
+++ src/vue/Tri.java
@@ -15,7 +15,6 @@ import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
 
 public class Tri extends JPanel {
 
