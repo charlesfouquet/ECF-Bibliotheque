f8d2a0bed13e5998fc214bf16fe3612c50f206ef
Karim <100566437+KarimARFI@users.noreply.github.com>
2023-02-13 09:45:32 +0100

    ajout méthode getConnect

--- src/sqlConnection/DBConnect.java
+++ src/sqlConnection/DBConnect.java
@@ -1,5 +1,23 @@
 package sqlConnection;
 
-public class DBConnect {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnect {
+	public static Connection getConnect() {
+		Connection myConnection = null;
+		
+		String url = "jdbc:mysql://localhost/";
+		String dbName = "ecf_bibliotheque";
+		String user = "root";
+		String password = "";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			myConnection = DriverManager.getConnection(url+dbName,user,password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return myConnection;
+	}
 }
