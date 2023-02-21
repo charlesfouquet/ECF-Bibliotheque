e19525f891377121501d7cb469369ef05861cc40
Karim <100566437+KarimARFI@users.noreply.github.com>
2023-02-21 22:04:56 +0100

    vue backoffice finalisé

--- src/vue/BackOffice.java
+++ src/vue/BackOffice.java
@@ -2,8 +2,8 @@ package vue;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,17 +11,17 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SpinnerDateModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 import controler.BackOfficeDAO;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class BackOffice extends JPanel {
 	BackOfficeDAO backOffice = new BackOfficeDAO();
@@ -34,12 +34,12 @@ public class BackOffice extends JPanel {
 	private JTextField theme;
 	private JTextField nomSerie;
 	private JTextField nomSocial;
-	private JTextField ISBN_livre;
 	private JTextField prenom;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_5;
+	private JTextField ISBNCm;
+	private JTextField titreCm;
+	private JTextField resumeCm;
+	private JTextField couvCm;
+	private JTable tableExemplaire;
 
 	/**
 	 * Create the panel.
@@ -58,7 +58,7 @@ public class BackOffice extends JPanel {
 		add(panelTitre);
 		
 		JLabel labelBackOffice = new JLabel("BACK OFFICE");
-		labelBackOffice.setFont(new Font("Dialog", Font.PLAIN, 18));
+		labelBackOffice.setFont(new Font("Noto serif", Font.PLAIN, 18));
 		labelBackOffice.setForeground(new Color(255, 255, 255));
 		panelTitre.add(labelBackOffice);
 		
@@ -70,32 +70,32 @@ public class BackOffice extends JPanel {
 		add(panel1);
 		panel1.setLayout(null);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(224, 11, 251, 28);
-		panel1.add(comboBox);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Je créer un nouveau livre !", "ISBN N° 1", "ISBN N° 2", "ISBN N° 3", "ISBN N° 4"}));
-		
-		JLabel labelZone1 = new JLabel("1- Création ou modification ?");
-		labelZone1.setFont(new Font("Dialog", Font.PLAIN, 16));
-		labelZone1.setBounds(10, 15, 215, 20);
-		panel1.add(labelZone1);
+		JLabel titreLabelZone1 = new JLabel("<html><b style=\"color:red\">1 -</b> Création ou modification ?</html>");
+		titreLabelZone1.setFont(new Font("Noto serif", Font.PLAIN, 16));
+		titreLabelZone1.setBounds(10, 15, 215, 20);
+		panel1.add(titreLabelZone1);
+
+		JComboBox comboBox1 = new JComboBox();
+		comboBox1.setBounds(224, 11, 251, 28);
+		panel1.add(comboBox1);
+		comboBox1.setModel(new DefaultComboBoxModel(new String[] {"Je créer un nouveau livre !", "ISBN N° 1", "ISBN N° 2", "ISBN N° 3", "ISBN N° 4"}));
 		
 		/*######################*/
 		/* ### PANEL ETAPE 2 ### */
 		/*######################*/
 		JPanel panel2 = new JPanel();
 		panel2.setBackground(new Color(250, 243, 230));
-		panel2.setBounds(10, 100, 485, 440);
+		panel2.setBounds(10, 100, 485, 223);
 		add(panel2);
 		panel2.setLayout(null);
 		
-		JLabel labelZone2 = new JLabel("2 - Création des éléments :");
-		labelZone2.setFont(new Font("Dialog", Font.PLAIN, 16));
-		labelZone2.setHorizontalAlignment(SwingConstants.LEFT);
-		labelZone2.setBounds(10, 11, 454, 30);
-		panel2.add(labelZone2);
+		JLabel titreLabelZone2 = new JLabel("<html><b style=\"color:red\">2 -</b> Création des éléments :</html>");
+		titreLabelZone2.setFont(new Font("Noto Serif", Font.PLAIN, 16));
+		titreLabelZone2.setHorizontalAlignment(SwingConstants.LEFT);
+		titreLabelZone2.setBounds(10, 11, 200, 30);
+		panel2.add(titreLabelZone2);
 		
-		JLabel auteurs= new JLabel("<html><b>Auteur :</b><br> <i>nom, prénom, bio</i></html>");
+		JLabel auteurs= new JLabel("<html><b>Auteur :</b><br> <i>nom, prénom</i></html>");
 		auteurs.setFont(new Font("Noto Serif", Font.PLAIN, 10));
 		auteurs.setBounds(10, 52, 95, 30);
 		panel2.add(auteurs);
@@ -140,15 +140,11 @@ public class BackOffice extends JPanel {
 		theme.setBounds(103, 92, 285, 30);
 		panel2.add(theme);
 		theme.setColumns(10);
-					
+		
 		JButton btnGenre = new JButton("Ajouter");
 		btnGenre.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnGenre.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+				//TODO INSERT INTO avec méthode générique
 			}
 		});
 		btnGenre.setBackground(new Color(90, 205, 25));
@@ -161,14 +157,15 @@ public class BackOffice extends JPanel {
 		series.setBounds(10, 132, 83, 30);
 		panel2.add(series);
 		
+		nomSerie = new JTextField();
+		nomSerie.setBounds(103, 132, 285, 30);
+		panel2.add(nomSerie);
+		nomSerie.setColumns(10);
+
 		JButton btnSerie = new JButton("Ajouter");
 		btnSerie.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnSerie.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+				//TODO INSERT INTO avec méthode générique
 			}
 		});
 		btnSerie.setBackground(new Color(90, 205, 25));
@@ -176,11 +173,6 @@ public class BackOffice extends JPanel {
 		btnSerie.setBounds(398, 136, 77, 23);
 		panel2.add(btnSerie);
 		
-		nomSerie = new JTextField();
-		nomSerie.setBounds(103, 132, 285, 30);
-		panel2.add(nomSerie);
-		nomSerie.setColumns(10);
-							
 		JLabel editeurs = new JLabel("<html><b>Editeur :</b><br> <i>nomSocial</i></html>");
 		editeurs.setFont(new Font("Noto Serif", Font.PLAIN, 10));
 		editeurs.setBounds(10, 172, 83, 30);
@@ -194,6 +186,7 @@ public class BackOffice extends JPanel {
 		JButton btnEditeur = new JButton("Ajouter");
 		btnEditeur.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//TODO INSERT INTO avec méthode générique
 			}
 		});
 		btnEditeur.setBackground(new Color(90, 205, 25));
@@ -201,150 +194,190 @@ public class BackOffice extends JPanel {
 		btnEditeur.setBounds(398, 176, 77, 23);
 		panel2.add(btnEditeur);
 		
-		JLabel exemplaires = new JLabel("<html><b>Exemplaire :</b><br> <i>ISBN_livre</i></html>");
-		exemplaires.setFont(new Font("Noto Serif", Font.PLAIN, 10));
-		exemplaires.setBounds(10, 212, 83, 30);
-		panel2.add(exemplaires);
-		
-		ISBN_livre = new JTextField();
-		ISBN_livre.setBounds(103, 212, 285, 30);
-		panel2.add(ISBN_livre);
-		ISBN_livre.setColumns(10);
-		
-		JButton btnExemplaire = new JButton("Ajouter");
-		btnExemplaire.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnExemplaire.setBackground(new Color(90, 205, 25));
-		btnExemplaire.setFont(new Font("Noto Serif", Font.PLAIN, 10));
-		btnExemplaire.setBounds(398, 216, 77, 23);
-		panel2.add(btnExemplaire);
-		
 		/*######################*/
 		/* ### PANEL ETAPE 3 ### */
 		/*######################*/
+		
 		JPanel panel3 = new JPanel();
 		panel3.setBackground(new Color(250, 243, 230));
-		panel3.setLayout(null);
-		panel3.setBounds(505, 41, 485, 499);
+		panel3.setBounds(10, 334, 485, 206);
 		add(panel3);
+		panel3.setLayout(null);
 		
-		JLabel labelZone3 = new JLabel("3 - Création ou Modification d'un livre :");
-		labelZone3.setFont(new Font("Noto Serif", Font.PLAIN, 16));
-		labelZone3.setHorizontalAlignment(SwingConstants.LEFT);
-		labelZone3.setBounds(10, 11, 275, 30);
-		panel3.add(labelZone3);
+		JLabel titreabelZone3 = new JLabel("<html><b style=\"color:red\">3 -</b> Gestion des exemplaires :</html>");
+		titreabelZone3.setHorizontalAlignment(SwingConstants.LEFT);
+		titreabelZone3.setFont(new Font("Noto Serif", Font.PLAIN, 16));
+		titreabelZone3.setBounds(10, 12, 224, 30);
+		panel3.add(titreabelZone3);
+		
+		JComboBox comboBoxExemplaire = new JComboBox();
+		comboBoxExemplaire.setModel(new DefaultComboBoxModel(new String[] {"Sélectionner un ISBN"}));
+		comboBoxExemplaire.setBounds(10, 52, 395, 30);
+		panel3.add(comboBoxExemplaire);
+
+		JButton btnPlus1 = new JButton("+ 1");
+		btnPlus1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO ajout d'un exemplaire selon ISBN dans le comboBox
+			}
+		});
+		btnPlus1.setFont(new Font("Noto Serif", Font.PLAIN, 10));
+		btnPlus1.setBackground(new Color(90, 205, 25));
+		btnPlus1.setBounds(415, 52, 60, 30);
+		panel3.add(btnPlus1);
+		
+		
+		JScrollPane scrollPaneExemplaire = new JScrollPane();
+		scrollPaneExemplaire.setBounds(10, 93, 465, 102);
+		panel3.add(scrollPaneExemplaire);
+		
+		tableExemplaire = new JTable();
+		tableExemplaire.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+			},
+			new String[] {
+				"id exemplaire", "ISBN", "Titre"
+			}
+		) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -6218395721389455182L;
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		tableExemplaire.getColumnModel().getColumn(0).setPreferredWidth(20);
+		tableExemplaire.getColumnModel().getColumn(1).setPreferredWidth(60);
+		scrollPaneExemplaire.setViewportView(tableExemplaire);
+		
+		/*######################*/
+		/* ### PANEL ETAPE 4 ### */
+		/*######################*/
+		JPanel panel4 = new JPanel();
+		panel4.setBackground(new Color(250, 243, 230));
+		panel4.setLayout(null);
+		panel4.setBounds(505, 41, 485, 499);
+		add(panel4);
+		
+		JLabel titreLabelZone4 = new JLabel("<html><b style=\"color:red\">4 -</b> Création ou Modification d'un livre :");
+		titreLabelZone4.setFont(new Font("Noto Serif", Font.PLAIN, 16));
+		titreLabelZone4.setHorizontalAlignment(SwingConstants.LEFT);
+		titreLabelZone4.setBounds(10, 11, 275, 30);
+		panel4.add(titreLabelZone4);
 		
 		JLabel ISBNLivre = new JLabel("ISBN : ");
 		ISBNLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		ISBNLivre.setBounds(20, 52, 115, 30);
-		panel3.add(ISBNLivre);
+		panel4.add(ISBNLivre);
+		
+		ISBNCm = new JTextField();
+		ISBNCm.setColumns(10);
+		ISBNCm.setBounds(145, 52, 319, 30);
+		panel4.add(ISBNCm);
 		
 		JLabel titreLivre = new JLabel("Titre :");
 		titreLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		titreLivre.setBounds(20, 93, 115, 30);
-		panel3.add(titreLivre);
+		panel4.add(titreLivre);
+		
+		titreCm = new JTextField();
+		titreCm.setColumns(10);
+		titreCm.setBounds(145, 93, 319, 30);
+		panel4.add(titreCm);
 		
 		JLabel resumeLivre = new JLabel("Résumé :");
 		resumeLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		resumeLivre.setBounds(20, 134, 115, 30);
-		panel3.add(resumeLivre);
+		panel4.add(resumeLivre);
+
+		resumeCm = new JTextField();
+		resumeCm.setColumns(10);
+		resumeCm.setBounds(145, 134, 319, 30);
+		panel4.add(resumeCm);
 		
 		JLabel datePubliLivre = new JLabel("Datede publication : ");
 		datePubliLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		datePubliLivre.setBounds(20, 175, 115, 30);
-		panel3.add(datePubliLivre);
+		panel4.add(datePubliLivre);
+
+		JSpinner dateCm = new JSpinner();
+		dateCm.setModel(new SpinnerDateModel(new Date(-2208992400000L), new Date(-2208992400000L), new Date(4133890800000L), Calendar.DAY_OF_WEEK_IN_MONTH));
+		dateCm.setBounds(145, 175, 319, 30);
+		panel4.add(dateCm);
 		
 		JLabel nbrPagesLivre = new JLabel("Nbr de pages :");
 		nbrPagesLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		nbrPagesLivre.setBounds(20, 216, 115, 30);
-		panel3.add(nbrPagesLivre);
+		panel4.add(nbrPagesLivre);
+		
+		JSpinner nbPageCm = new JSpinner();
+		nbPageCm.setModel(new SpinnerNumberModel(Integer.valueOf(1), Integer.valueOf(1), null, Integer.valueOf(1)));
+		nbPageCm.setBounds(145, 216, 319, 26);
+		panel4.add(nbPageCm);
 		
 		JLabel couvLivre = new JLabel("Couverture :");
 		couvLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		couvLivre.setBounds(20, 257, 115, 30);
-		panel3.add(couvLivre);
+		panel4.add(couvLivre);
+
+		couvCm = new JTextField();
+		couvCm.setColumns(10);
+		couvCm.setBounds(145, 257, 319, 30);
+		panel4.add(couvCm);
 		
 		JLabel auteurLivre = new JLabel("Auteur :");
 		auteurLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
-		auteurLivre.setBounds(20, 298, 115, 30);
-		panel3.add(auteurLivre);
+		auteurLivre.setBounds(20, 298, 59, 30);
+		panel4.add(auteurLivre);
+
+		JComboBox auteurCm = new JComboBox();
+		auteurCm.setModel(new DefaultComboBoxModel(new String[] {"Choisissez un auteur", "auteur 1", "auteur 2"}));
+		auteurCm.setBounds(89, 298, 375, 30);
+		panel4.add(auteurCm);
 		
 		JLabel genreLivre = new JLabel("Genre :");
 		genreLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
-		genreLivre.setBounds(20, 339, 115, 30);
-		panel3.add(genreLivre);
+		genreLivre.setBounds(20, 339, 59, 30);
+		panel4.add(genreLivre);
+		
+		JComboBox genreCm = new JComboBox();
+		genreCm.setModel(new DefaultComboBoxModel(new String[] {"Choisissez un thème", "thème 1", "thème 2"}));
+		genreCm.setBounds(89, 339, 375, 30);
+		panel4.add(genreCm);
 		
 		JLabel serieLivre = new JLabel("Serie :");
 		serieLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
-		serieLivre.setBounds(20, 380, 115, 30);
-		panel3.add(serieLivre);
+		serieLivre.setBounds(20, 380, 59, 30);
+		panel4.add(serieLivre);
+
+		JComboBox serieCm = new JComboBox();
+		serieCm.setModel(new DefaultComboBoxModel(new String[] {"Choisissez une serie", "null (Livre ne faisant as partie d'une série)", "serie 1", "serie 2"}));
+		serieCm.setBounds(89, 380, 214, 30);
+		panel4.add(serieCm);
+		
+		JLabel labelVolumeCm = new JLabel("+ son Volume N° :");
+		labelVolumeCm.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelVolumeCm.setBounds(313, 380, 87, 30);
+		panel4.add(labelVolumeCm);
+		
+		JSpinner volNumCm = new JSpinner();
+		volNumCm.setBounds(410, 380, 54, 30);
+		panel4.add(volNumCm);
 		
 		JLabel editeurLivre = new JLabel("Editeur :");
 		editeurLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
-		editeurLivre.setBounds(20, 421, 115, 30);
-		panel3.add(editeurLivre);
+		editeurLivre.setBounds(20, 421, 59, 30);
+		panel4.add(editeurLivre);
 		
-		JLabel exempLivre = new JLabel("Exemplaire :");
-		exempLivre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
-		exempLivre.setBounds(20, 462, 115, 30);
-		panel3.add(exempLivre);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(145, 52, 319, 30);
-		panel3.add(textField);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(145, 93, 319, 30);
-		panel3.add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(145, 134, 319, 30);
-		panel3.add(textField_2);
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(145, 257, 319, 30);
-		panel3.add(textField_5);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Choisissez un auteur", "auteur 1", "auteur 2"}));
-		comboBox_1.setBounds(145, 298, 319, 30);
-		panel3.add(comboBox_1);
-		
-		JComboBox comboBox_1_1 = new JComboBox();
-		comboBox_1_1.setModel(new DefaultComboBoxModel(new String[] {"Choisissez un thème", "thème 1", "thème 2"}));
-		comboBox_1_1.setBounds(145, 339, 319, 30);
-		panel3.add(comboBox_1_1);
-		
-		JComboBox comboBox_1_1_1 = new JComboBox();
-		comboBox_1_1_1.setModel(new DefaultComboBoxModel(new String[] {"Choisissez une serie", "null (Livre ne faisant as partie d'une série)", "serie 1", "serie 2\t"}));
-		comboBox_1_1_1.setBounds(145, 380, 319, 30);
-		panel3.add(comboBox_1_1_1);
-		
-		JComboBox comboBox_1_1_1_1 = new JComboBox();
-		comboBox_1_1_1_1.setModel(new DefaultComboBoxModel(new String[] {"Choisissez un éditeur", "editeur 1", "editeur 2"}));
-		comboBox_1_1_1_1.setBounds(145, 421, 319, 30);
-		panel3.add(comboBox_1_1_1_1);
-		
-		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(1, 1, 5, 1));
-		spinner.setBounds(145, 462, 319, 26);
-		panel3.add(spinner);
-		
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setModel(new SpinnerDateModel(new Date(-2208992400000L), new Date(-2208992400000L), new Date(4133890800000L), Calendar.DAY_OF_WEEK_IN_MONTH));
-		spinner_1.setBounds(145, 175, 319, 30);
-		panel3.add(spinner_1);
-		
-		JSpinner spinner_2 = new JSpinner();
-		spinner_2.setModel(new SpinnerNumberModel(Integer.valueOf(1), Integer.valueOf(1), null, Integer.valueOf(1)));
-		spinner_2.setBounds(145, 216, 319, 26);
-		panel3.add(spinner_2);
+		JComboBox editeurCm = new JComboBox();
+		editeurCm.setModel(new DefaultComboBoxModel(new String[] {"Choisissez un éditeur", "editeur 1", "editeur 2"}));
+		editeurCm.setBounds(89, 421, 375, 30);
+		panel4.add(editeurCm);
 		
 		JButton btnValideLIvre = new JButton("Je valide mon livre !");
 		btnValideLIvre.addActionListener(new ActionListener() {
@@ -353,8 +386,8 @@ public class BackOffice extends JPanel {
 		});
 		btnValideLIvre.setFont(new Font("Noto Serif", Font.PLAIN, 12));
 		btnValideLIvre.setBackground(new Color(90, 205, 25));
-		btnValideLIvre.setBounds(314, 11, 150, 30);
-		panel3.add(btnValideLIvre);
+		btnValideLIvre.setBounds(314, 462, 150, 30);
+		panel4.add(btnValideLIvre);
 		
 	}
 }
